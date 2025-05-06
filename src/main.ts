bd8c7e6fe24bf8195b2ee89209f19a0b04375891
Victor Abreu <aalevictor@gmail.com>
2025-05-06 17:10:37 -0300

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   const port = process.env.PORT || 3000;
-  app.enableCors({ origin: 'http://localhost:3001' });
+  app.enableCors({ origin: process.env.CORS_ORIGIN || 'http://localhost:3001' });
   const options = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Atendimento ao Público - Agendamentos')
